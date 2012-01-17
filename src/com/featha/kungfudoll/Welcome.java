e327493a8196fbf6af896e3e6442171caea35ea1
David Li <taweili@yahoo.com>
2012-01-17 23:42:44 +0800

    complete JS integration

--- src/com/featha/kungfudoll/Welcome.java
+++ src/com/featha/kungfudoll/Welcome.java
@@ -1,13 +1,93 @@
 package com.featha.kungfudoll;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 
-public class Welcome extends Activity {
+import com.phonegap.*;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.PluginState;
+
+public class Welcome extends DroidGap {
+	
+	private ServerSocket serverSocket;
+    private Handler handler = new Handler();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+		Thread st = new Thread(new ServerThread());
+		st.start();
+        
+        super.loadUrl("file:///android_asset/www/index.html");
+        
+        WebSettings settings = appView.getSettings(); 
+      	settings.setPluginState(PluginState.ON);
+      	
+        Log.d("welcome", "appView: " + appView);
+        Log.d("socket", "local address" + getLocalIpAddress());
     }
+    
+	private String getLocalIpAddress() {
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()) {
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		} catch (SocketException ex) {
+			Log.e("ServerActivity", ex.toString());
+		}
+		return null;
+	}
+
+    
+	public class ServerThread implements Runnable {
+		private String line = null;
+
+		public void run() {
+			try {
+				serverSocket = new ServerSocket(8080);
+				while (true) {
+					Socket client = serverSocket.accept();
+
+					try {
+						BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        while ((line = in.readLine()) != null) {
+        					handler.post(new Runnable() {
+        						public void run() {
+        							String str[] = line.split(" ");
+        							Log.d("socket", str[0] + " - " + str[1]);
+        							appView.loadUrl("javascript:$('#score').text('2');");
+        							appView.loadUrl("javascript:$('#speed').text('200');");
+        						}
+        					});                        	
+                        }
+					} catch (Exception e) {
+						Log.e("socket", e.getMessage());
+					}
+					client.close();
+				}
+			} catch (Exception e) {
+				Log.e("socket", "exception: " + e.getMessage());
+			}
+		}
+
+	}
+
 }
